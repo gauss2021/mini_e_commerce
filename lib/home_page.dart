a1b69ef6494417251215f8fd9e4df132148d5227
gauss2021 <bioussalavandavydson@gmail.com>
2024-02-07 16:24:07 +0100

    Add the chip widget for display the brands

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -5,18 +5,59 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    final List<String> brands = ['Addidas', 'Nike', 'Puma', 'New Balance'];
+    return Scaffold(
       body: SafeArea(
           child: Column(
         children: [
-          Row(
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Text(
-                "Shopping Express",
-                style:
-                    TextStyle(fontFamily: 'Lato', fontWeight: FontWeight.bold),
+              Padding(
+                padding: EdgeInsets.all(12.0),
+                child: Text(
+                  "Shopping \n Collection",
+                  style: TextStyle(
+                      fontFamily: 'Lato',
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30),
+                ),
+              ),
+              Expanded(
+                child: TextField(
+                  decoration: InputDecoration(
+                      hintText: 'Search',
+                      prefixIcon: Icon(Icons.search),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(30),
+                            bottomLeft: Radius.circular(30)),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Color.fromRGBO(0, 0, 0, 0.6)),
+                          borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(30),
+                              bottomLeft: Radius.circular(30)))),
+                ),
               )
             ],
+          ),
+          const SizedBox(
+            height: 12,
+          ),
+          SizedBox(
+            height: 50,
+            child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: brands.length,
+                itemBuilder: (context, index) {
+                  String brandLabel = brands[index];
+                  return Padding(
+                    padding: const EdgeInsets.only(right: 12),
+                    child: Chip(label: Text(brandLabel)),
+                  );
+                }),
           )
         ],
       )),
